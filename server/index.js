d14ae3455817980928a4d4d5193b476419c776a6
Rupak Das <133593360+RupakDas62@users.noreply.github.com>
2025-06-19 21:57:05 +0530

    Changes in env file

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const fetch = require('node-fetch');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
